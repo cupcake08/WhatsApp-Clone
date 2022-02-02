3b71200e5e7b6b42edc1ca01b47b8b567d5554f6
cupcake08 <bhankhariaa@gmail.com>
2022-02-02 13:38:36 +0530

    constant color variables

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
 
-const Color tealGreen = Color.fromRGBO(18, 140, 126, 1);
+const Color tealGreen = Color.fromRGBO(18, 200, 126, 1);
 const Color tealGreenDark = Color.fromRGBO(7, 94, 84, 1);
 
 const Color grey = Color.fromRGBO(69, 90, 100, 1);
