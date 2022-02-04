066c116e97cd9b96a863ca1e84e150918637502c
cupcake08 <bhankhariaa@gmail.com>
2022-02-04 18:51:29 +0530

    const change

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -13,25 +13,22 @@ class Message {
 }
 
 const List<Message> messageList = [
-  const Message(
-      msg: "You Hi Nigga", time: "2:30 pm", date: "21 december, 2021"),
-  const Message(
+  Message(msg: "You Hi Nigga", time: "2:30 pm", date: "21 december, 2021"),
+  Message(
       msg: "You should Check this out.",
       time: "2:30 pm",
       date: "21 december, 2021"),
-  const Message(
+  Message(
       msg: "Let me See",
       time: "2:30 pm",
       date: "21 december, 2021",
       isSender: true),
-  const Message(
-      msg: "Yo that's cool", time: "2:30 pm", date: "21 december, 2021"),
-  const Message(
+  Message(msg: "Yo that's cool", time: "2:30 pm", date: "21 december, 2021"),
+  Message(
       msg: "Cool", time: "2:30 pm", date: "21 december, 2021", isSender: true),
-  const Message(msg: "Ok yeah", time: "2:30 pm", date: "21 december, 2021"),
-  const Message(
-      msg: "see ya next time!", time: "2:30 pm", date: "21 december, 2021"),
-  const Message(msg: "Bye!", time: "2:30 pm", date: "21 december, 2021"),
-  const Message(
+  Message(msg: "Ok yeah", time: "2:30 pm", date: "21 december, 2021"),
+  Message(msg: "see ya next time!", time: "2:30 pm", date: "21 december, 2021"),
+  Message(msg: "Bye!", time: "2:30 pm", date: "21 december, 2021"),
+  Message(
       msg: "Bye!", time: "2:30 pm", date: "21 december, 2021", isSender: true),
 ];
