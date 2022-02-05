fab1615e611105e5d2f0729fcca29ec6e3d0abcb
cupcake08 <bhankhariaa@gmail.com>
2022-02-05 09:57:11 +0530

    padding change

--- lib/widgets/message_card.dart
+++ lib/widgets/message_card.dart
@@ -7,35 +7,38 @@ class MessageCard extends StatelessWidget {
   final Message msg;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment:
-          msg.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(top: 10),
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-          child: Row(
-            children: [
-              Text(
-                msg.msg,
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(
-                width: 4,
-              ),
-              Text(
-                msg.time,
-                style:
-                    const TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        mainAxisAlignment:
+            msg.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            child: Row(
+              children: [
+                Text(
+                  msg.msg,
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  msg.time,
+                  style: const TextStyle(
+                      fontSize: 10, fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: msg.isSender ? tealGreen : tealGreenDark,
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
-          decoration: BoxDecoration(
-            color: msg.isSender ? tealGreen : tealGreenDark,
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
